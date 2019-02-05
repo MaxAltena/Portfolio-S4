b98e11dae474638f1ac50b8d41087ad2ec3ac42b
MaxAltena <Max@MaxAltena.com>
2019-02-05 01:18:56 +0100

    added google drive pdfs

--- src/App.js
+++ src/App.js
@@ -1,75 +1,156 @@
 import React, { Component } from "react";
-import SideMenu from "react-sidemenu";
+import Navigation from "./components/Navigation";
+import Content from "./components/Content";
+import validateHash from "./utils/validateHash";
 import "./App.css";
-import "../node_modules/react-sidemenu/dist/side-menu.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
+    const { hash } = this.props.location;
+    const hashStripped = hash.substr(1);
+    const sidemenuItems = [
+      {
+        divider: true,
+        label: "Algemeen",
+        value: "algemeen"
+      },
+      {
+        label: "Home",
+        value: "home",
+        icon: "fa-home"
+      },
+      { label: "Kennismaking", value: "kennismaking", icon: "fa-comment" },
+      {
+        label: "item 2",
+        value: "item2",
+        icon: "fa-award",
+        children: [
+          {
+            label: "item 2.1",
+            value: "item2.1",
+            icon: "fa-shower",
+            children: [
+              { label: "item 2.1.1", value: "item2.1.1", icon: "fa-bed" },
+              { label: "item 2.1.2", value: "item2.1.2", icon: "fa-bolt" }
+            ]
+          },
+          { label: "item 2.2", value: "item2.2", icon: "fa-bomb" }
+        ]
+      },
+      {
+        label: "item 3",
+        value: "item3",
+        icon: "fa-bong",
+        children: [
+          {
+            label: "item 3.1",
+            value: "item3.1",
+            icon: "fa-adjust",
+            children: [
+              {
+                label: "item 3.1.1",
+                value: "item3.1.1",
+                icon: "fa-anchor"
+              },
+              {
+                label: "item 3.1.2",
+                value: "item3.1.2",
+                icon: "fa-ankh"
+              }
+            ]
+          },
+          { label: "item 3.2", value: "item3.2", icon: "fa-bath" }
+        ]
+      },
+      { divider: true, label: "Strategie & Concept", value: "nav-SCO" },
+      { label: "Vak", value: "sco1", icon: "fa-beer" },
+      {
+        divider: true,
+        label: "User Experience & User Centered Design",
+        value: "nav-UXU"
+      },
+      { label: "Vak", value: "uxu1", icon: "fa-beer" },
+      { divider: true, label: "Design & Development", value: "nav-DED" },
+      { label: "Vak", value: "ded1", icon: "fa-beer" }
+    ];
+    const validHash = validateHash(hash, sidemenuItems);
+
+    if (!validHash) {
+      this.props.history.push("/#home");
+    }
+
     this.state = {
-      sidemenu: [
-        { divider: true, label: "Main navigation", value: "main-nav" },
+      hash,
+      hashStripped,
+      validHash,
+      sidemenu: {
+        items: sidemenuItems,
+        theme: "custom",
+        activeItem: hashStripped
+      },
+      content: [
+        { hash: "home", type: "page" },
+
         {
-          label: "item 1",
-          value: "item1",
-          icon: "fa-search",
-          children: [
-            {
-              label: "item 1.1",
-              value: "item1.1",
-              icon: "fa-adjust",
-              children: [
-                { label: "item 1.1.1", value: "item1.1.1", icon: "fa-anchor" },
-                {
-                  label: "item 1.1.2",
-                  value: "item1.1.2",
-                  icon: "fa-ankh"
-                }
-              ]
-            },
-            { label: "item 1.2", value: "item1.2", icon: "fa-bath" }
-          ]
+          hash: "kennismaking",
+          type: "iframe",
+          content:
+            "https://drive.google.com/file/d/1Bc2B8H6ydJK8YMvnhq9jzdu7EER3EwquZu1oJuO0lxg/preview"
         },
-        {
-          label: "item 2",
-          value: "item2",
-          icon: "fa-award",
-          children: [
-            {
-              label: "item 2.1",
-              value: "item2.1",
-              icon: "fa-shower",
-              children: [
-                { label: "item 2.1.1", value: "item2.1.1", icon: "fa-bed" },
-                { label: "item 2.1.2", value: "item2.1.2", icon: "fa-bolt" }
-              ]
-            },
-            { label: "item 2.2", value: "item2.2", icon: "fa-bomb" }
-          ]
-        },
-        { divider: true, label: "Strategie & Concept", value: "nav-SCO" },
-        { label: "Vak", value: "sco1", icon: "fa-beer" },
-        {
-          divider: true,
-          label: "User Experience & User Centered Design",
-          value: "nav-UXU"
-        },
-        { label: "Vak", value: "uxu1", icon: "fa-beer" },
-        { divider: true, label: "Design & Development", value: "nav-DED" },
-        { label: "Vak", value: "ded1", icon: "fa-beer" }
+        { hash: "item2", type: "page" },
+        { hash: "item2.1", type: "page" },
+        { hash: "item2.1.1", type: "page" },
+        { hash: "item2.1.2", type: "page" },
+        { hash: "item2.2", type: "page" },
+        { hash: "item3", type: "page" },
+        { hash: "item3.1", type: "page" },
+        { hash: "item3.1.1", type: "page" },
+        { hash: "item3.1.2", type: "page" },
+        { hash: "item3.2", type: "page" },
+        { hash: "sco1", type: "page" },
+        { hash: "uxu1", type: "page" },
+        { hash: "ded1", type: "page" }
       ]
     };
   }
+
+  componentDidUpdate = () => {
+    const { hash } = this.props.location;
+    const hashStripped = hash.substr(1);
+    const validHash = validateHash(hash, this.state.sidemenu.items);
+
+    if (!validHash) {
+      this.props.history.push("/#home");
+    } else {
+      if (this.state.hash !== this.props.location.hash) {
+        this.setState({
+          ...this.state,
+          hash,
+          hashStripped,
+          validHash,
+          sidemenu: {
+            ...this.state.sidemenu,
+            activeItem: hashStripped
+          }
+        });
+      }
+    }
+  };
+
   render() {
-    const { sidemenu } = this.state;
+    const { hashStripped, sidemenu, content } = this.state;
+
+    let currentItem = undefined;
+    content.forEach(item => {
+      if (item.hash === hashStripped) currentItem = item;
+    });
+
     return (
       <div className="App">
-        <SideMenu items={sidemenu} />
-        <main>
-          <h1 className="hind bold center">Portfolio-S4</h1>
-          <h2 className="center">Max Altena</h2>
-        </main>
+        <Navigation sidemenu={sidemenu} />
+        <Content item={currentItem} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route render={() => <Redirect to="/" />} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
